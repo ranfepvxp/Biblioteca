d794103cb0fc071162d7cea7ad9715b3f734c215
Ranferi <ranfepv@logsys.mx>
2024-06-25 09:22:33 -0600

    tomando los datos de feature de github

--- BibliotecaAPI/BibliotecaAPI/obj/Debug/net6.0/BibliotecaAPI.AssemblyInfo.cs
+++ BibliotecaAPI/BibliotecaAPI/obj/Debug/net6.0/BibliotecaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc24db6977b90d169b7e0846c9496480cd56289c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
